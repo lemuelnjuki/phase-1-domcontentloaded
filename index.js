4c311a5dc9115a8bc176180d7d43c0a71c6e6425
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-23 09:43:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    let  textId = document.getElementById('text');
+    textId.textContent  = "This is really cool!"
+});
+    
